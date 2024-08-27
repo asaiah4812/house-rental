03310a366cdb9eea4cd9b8ebc1b82bf8f3909d6b
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-08-27 02:55:09 -0700

    new update

--- altlets/settings.py
+++ altlets/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATICFILES_URLS = BASE_DIR/'static'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR/'staticfiles', 'static')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,16 @@
 # build_files.sh
 
-python -m pip install -r requirements.txt
-python 3.11.4 manage.py collectstatic
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python 3.11.4 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+
+pip install -r requirements.txt
+python 3.11.4 manage.py collectstatic --noinput
+
+
+echo "BUILD END"
