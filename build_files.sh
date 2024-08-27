c9582aa7fe2a9aa91e9ed6fe6c46c742e0406574
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-08-27 03:25:52 -0700

    psycopg2

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,5 @@
 # build_files.sh
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python 3.11.4 -m venv venv
 
-# activate the virtual environment
-source venv/bin/activate
-
-
-pip install -r requirements.txt
-python 3.11.4 manage.py collectstatic --noinput
-
+python -m pip install -r requirements.txt
+python 3.11.4 manage.py collectstatic
